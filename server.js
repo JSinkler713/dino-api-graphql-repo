e3a592ed6647002a734d2cbca175e9475b9789cb
James Sinkler <jamessinkler713@gmail.com>
2020-11-24 21:28:12 -0800

    adds apollo server

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+const jsonParser = express.json()
+const db = require('./models');
+
+
+const { ApolloServer } = require('apollo-server')
+//create a schema
+const gql = require('graphql-tag')
+
+//
+
+const typeDefs = require('./typedefs.js') 
+// resolvers
+const resolvers = require('./resolvers.js')
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context() {
+    return db 
+  }
+})
+
+server.listen().then(({url})=>console.log(`server on ${url}`))
+
+
+
+
+
+
+
+// Set View Engine (Server Rendered Templates)
+
+//method_override
+
+
+//require dinosaurController
