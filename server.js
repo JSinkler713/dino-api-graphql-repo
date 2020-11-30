898822a2690d5053dd82146a43a2a00db7bb80fe
James Sinkler <jamessinkler713@gmail.com>
2020-11-30 11:54:26 -0800

    allows introspection and playgorund with deployed app

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ const server = new ApolloServer({
   resolvers,
   context() {
     return db 
-  }
+  },
+  introspection: true,
+  playground: true,
 })
 
 server.listen({ port: process.env.PORT || 4000}).then(({url})=>console.log(`server on ${url}`))
